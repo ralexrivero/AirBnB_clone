34e4babb63f803f20e24bd557125ae616daecf8e
RodrigoMato00 <3293@holbertonschool.com>
2021-11-08 07:51:35 -0800

    correccion de comentarios en console.py

--- console.py
+++ console.py
@@ -17,13 +17,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, args):
         """Quit command to exit the program"""
-        'Quit command to exit the program'
 
         quit()
 
     def do_EOF(self, args):
         """End Of File command to exit the program"""
-        'End Of File command to exit the program EOF'
 
         quit()
 
