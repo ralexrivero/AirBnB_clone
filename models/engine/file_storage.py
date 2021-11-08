80d74e39c950c1bdbffeb55b0363e4a0142dc71b
ralexrivero <ralexrivero@gmail.com>
2021-11-08 15:42:17 -0300

    create FileStorage Class empty

---
+++ models/engine/file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Class FileStorage
+"""
+
+import json
+
+class FileStorage:
+    """ Class FileStorage"""
+    print("FileStorage class")
+
+    def reload(self):
+        """reload method"""
+        pass
