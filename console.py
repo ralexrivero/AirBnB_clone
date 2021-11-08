427038aad04b50c8fc887107a6e75ef2c143aceb
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-08 14:55:35 -0300

    crate instance- terminado...

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """
 Console for object management and storage persistant
 """
-
+from models import *
 import os
 import sys
 import json
@@ -14,6 +14,9 @@ class HBNBCommand(cmd.Cmd):
     """consola HBNB"""
 
     prompt = "(hbnb) "
+    my_classes = {"BaseModel": BaseModel, "User": User, "State": State,
+                   "City": City, "Amenity": Amenity, "Place": Place}
+    # my_classes is a diccionary with the classes
 
     def do_quit(self, args):
         """Quit command to exit the program"""
@@ -32,6 +35,18 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
 
-            
+        args = args.split() # divides a args into a list
+        if len.args == 0:
+            print("** class name missing **")
+            return
+        else:
+            if len(args) > 0 and args[0] not in HBNBCommand.my_classes:
+                print("** class doesn't exist **")
+            else:
+                len(args) > 0 and args[0] in HBNBCommand.my_classes:
+                    new_object = HBNBCommand.my_classes[args[0]]()
+                    new_object.save()
+                    print(new_object.id)
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
