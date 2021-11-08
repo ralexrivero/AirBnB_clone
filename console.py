19566b033375d1da226f3f2ddcc7bb3d8bba7fb1
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-08 13:06:46 -0300

    create instance en proceso

--- console.py
+++ console.py
@@ -24,6 +24,14 @@ class HBNBCommand(cmd.Cmd):
         """End Of File command to exit the program"""
 
         quit()
+    
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id."""
 
+        if not args:
+            print("** class name missing **")
+            return
+
+            
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
