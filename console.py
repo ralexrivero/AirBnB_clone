6079e1b3c2592106e0b78804fb620dab568329f5
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-08 12:21:05 -0300

    correccion de identacion

--- console.py
+++ console.py
@@ -14,13 +14,13 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-def do_quit(self, arg):
-    """Quit command to exit the program"""
-    quit()
+    def do_quit(self, args):
+        """Quit command to exit the program"""
+        quit()
 
-def do_EOF(self, arg):
-    """EOF command to exit the program"""
-    quit()
+    def do_EOF(self, args):
+        """EOF command to exit the program"""
+        quit()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
