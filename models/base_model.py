e82741a2b36ef3c7c4d4252e0a1b14a082de59cc
ralexrivero <ralexrivero@gmail.com>
2021-11-11 16:01:15 -0300

    apply pep8 style

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ instantiates a new object """
-       
+
         if len(args) > 0:
             for i in args[0]:
                 setattr(self, i, args[0][i])
@@ -18,27 +18,14 @@ class BaseModel:
                 the name of the attribute to be set and the value to be
                 assigned to it.
                 """
-       
+
         else:
-       
+
             self.created_at = datetime.now()
             self.id = str(uuid4())
-       
+
         for i in kwargs:
             print("kwargs: {}: {}".format(i, kwargs[i]))
-
- #   def __init__(self, *args, **kwargs):
- #          """ Initialize a new BaseModel
- #           Args:
- #               *args: variable length argument list
- #               **kwargs: key - value pairs
- #       """
- #       self.id = str(uuid4)
- #       self.created_at = datetime.datetime.now()
- #       current datetime when instance is created
- #       self.updated_at = datetime.datetime.now()
- #       """current datetime when instance is created and
- #       updated when change object"""
 
     def __str__(self):
         """ Returns a string representation of the object """
@@ -61,5 +48,5 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-       
+
         return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,17 +15,20 @@ import os.path
 
 classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
            "Amenity": Amenity, "Place": Place, "Review": Review}
+
+
 class FileStorage:
     """ Class FileStorage"""
-    
+
     __file_path = "file.json"
     """__file_path is the path of the json file"""
     __objects = {}
     """__objects is a dictionnary of all objects"""
 
     def __init__(self):
-        """constructor metod of FileStorage class, initialize all the class attributes""" 
-        
+        """constructor metod of FileStorage class, initialize all
+        the class attributes"""
+
     def all(self):
         """Reeturn dictionnary of all objects"""
         return FileStorage.__objects
@@ -40,24 +43,28 @@ class FileStorage:
                 FileStorage.__objects[new_obj] = obj
 
     def save(self):
-        """serialize and save objects from __objects to a file in json in format json"""
+        """serialize and save objects from __objects to a file
+        in json in format json"""
         dictionary = {}
         """dicttionary is an empty dictionnary"""
         for key, value in FileStorage.__objects.items():
-                """this for loop utilise a key and valiu to run 
-                FileStorage.__objects.items() and create a dictioanry of key and value"""
-                dictionary[key] = value.to_dict()
+            """this for loop utilise a key and valiu to run
+            FileStorage.__objects.items() and create a dictioanry of
+            key and value"""
+            dictionary[key] = value.to_dict()
         """dict[key] is equal to value.__dict__"""
         with open(self.__file_path, 'w') as f:
-                """open(self.__file_path, 'w') open the json file in write mode"""
-                json.dump(dictionary, f)
-                """dump(dictionary, f) dump the dictionnary dictionary in the file f"""
-    
+            """open(self.__file_path, 'w') open the json file in write mode"""
+            json.dump(dictionary, f)
+            """dump(dictionary, f) dump the dictionnary in the file f"""
+
     def reload(self):
-        """deserialize and lode objects from the file into python objects to dictioanry __objects"""
+        """deserialize and lode objects from the file into python
+        objects to dictionary __objects"""
         try:
-            with open( self.__file_path, 'r') as f: 
-                """open( self.__file_path, 'r') open the json file in read mode"""
+            with open(self.__file_path, 'r') as f:
+                """open( self.__file_path, 'r') open the json file
+                in read mode"""
                 my_dict = json.load(f.read)
                 """load(f.read) load the file f and read it"""
             for key, value in my_dict.items():
@@ -69,7 +76,7 @@ class FileStorage:
                 if new_object in classes:
                     self.__objects[key] = classes[new_object](**value)
                     """this if statement is used to create a new object
-                        with the class name of new_object and the value of value"""
+                        with the class name of new_object and its value"""
         except:
             pass
         """except is used to catch the error if the file is empty"""
