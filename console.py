3e8fe00bf20a2c2778ee462ab73cfc309921e0f9
RodrigoMato00 <3293@holbertonschool.com>
2021-11-10 23:45:11 -0300

    arreglos en la consola, se le agrego el new line luego de los comentarios, file_storage aparentemente terminado

--- console.py
+++ console.py
@@ -45,25 +45,22 @@ class HBNBCommand(cmd.Cmd):
     instances = ["do_show", "do_destroy", "do_all", "do_update"] 
 
     def do_quit(self, args):
-        """Quit command to exit the program"""
+        """Quit command to exit the program.\n"""
 
         quit()
 
     def do_EOF(self, args):
-        """End Of File command to exit the program"""
+        """End Of File command to exit the program\n"""
 
         quit()
 
-    def emptyline(self, args):
-        """Do nothing on empty line"""
+    def emptyline(self):
+        """Do nothing on empty line\n"""
+
         pass
     
-    def default(self, args):
-        """Default method to execute when the command is not found"""
-        print("*** Unknown syntax: {}".format(args))
-    
     def do_create(self, args):
-        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id."""
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id.\n"""
 
         if not args:
             print("** class name missing **")
@@ -76,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
     def do_show(self, args):
-        """Prints the string representation of an instance, show BaseModel 1234-1234-1234"""
+        """Prints the string representation of an instance, show BaseModel 1234-1234-1234.\n"""
 
         args = args.split()
         """args.split() splits a string into a list of words."""
@@ -90,7 +87,7 @@ class HBNBCommand(cmd.Cmd):
 
         
     def do_destroy(self, args):
-        """ Deletes an instance based on the class name and id"""
+        """ Deletes an instance based on the class name and id.\n"""
 
         args_list = shlex.split(args)
         """args_list = shlex.split(args) splits a args_list into a list of words."""
@@ -104,19 +101,19 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         #if args_list[1] not in my_objects:       
-        #my_objects = storage.all()
+        my_objects = storage.all()
         if args_list[0] not in my_objects:
             print("** no instance found **")
             return
     
     def do_all(self, args):
-        """Prints all string representation of all instances based or not on the class name."""
+        """Prints all string representation of all instances based or not on the class name.\n"""
     def do_update(self, args):
         """Updates an instance based on the class name and id by adding or updating attribute
-        (save the change into the JSON file).
-        """
+        (save the change into the JSON file).\n"""
 
 if __name__ == "__main__":
     """__name__ is a special variable that holds the name of the current module and __main__ is the name of the
     "main" module; if this module is being run as the main module, __name__ will be __main__."""
     HBNBCommand().cmdloop()
+    """cmdloop() is a method that runs a command prompt command line interface for a Python program."""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,37 +19,57 @@ class FileStorage:
     """ Class FileStorage"""
     
     __file_path = "file.json"
+    """__file_path is the path of the json file"""
     __objects = {}
+    """__objects is a dictionnary of all objects"""
 
     def __init__(self):
         """constructor metod of FileStorage class, initialize all the class attributes""" 
-        self.__reload()
-
+        
     def all(self):
         """Reeturn dictionnary of all objects"""
         return FileStorage.__objects
+        """return a dictionnary of all objects"""
 
     def new(self, obj):
-        """add objecto to the dictionnary __objects"""
+        """add object to the dictionnary __objects"""
         if new_obj is None:
             for new_obj in FileStorage.__objects:
+                """this for loop utilise a new_obj to run
+                    the loop for each object in the dictionnary"""
                 FileStorage.__objects[new_obj] = obj
 
     def save(self):
         """serialize and save objects from __objects to a file in json in format json"""
         dict = {}
-        for i in FileStorage.__objects.keys():
-            dict[i] = FileStorage.__objects[i].to_json()
-        with open(self.__file_path, 'w') as file:
-            json.dump(dict, file)
- 
-   def reload(self):
+        """dict is an empty dictionnary"""
+        for key, value in FileStorage.__objects.items():
+                """this for loop utilise a key and valiu to run 
+                FileStorage.__objects.items() and create a dictioanry of key and value"""
+        dict[key] = value.to_dict()
+        """dict[key] is equal to value.__dict__"""
+        with open(self.__file_path, 'w') as f:
+                """open(self.__file_path, 'w') open the json file in write mode"""
+        json.dump(dict, f)
+        """dump(dict, f) dump the dictionnary dict in the file f"""
+    
+    def reload(self):
         """deserialize and lode objects from the file into python objects to dictioanry __objects"""
-
         try:
-            with open(self.__file_path, 'r') as file:
-                jeison = json.load(file)
-            for i in jeison:
-                self.__objects[i] = classes[jeison[i]["__class__"]](**jeison[i])
+            with open( self.__file_path, 'r') as f: 
+                """open( self.__file_path, 'r') open the json file in read mode"""
+                my_dict = json.load(f.read)
+                """load(f.read) load the file f and read it"""
+            for key, value in my_dict.items():
+                """this for loop utilise a key and valiu to run
+                    my_dict.items() and create a dictioanry of key and value"""
+                new_object = key.split('.')[0]
+                """new_object is equal to key.split('.')[0]
+                    this split the key and take the first part of the key"""
+                if new_object in classes:
+                    self.__objects[key] = classes[new_object](**value)
+                    """this if statement is used to create a new object
+                        with the class name of new_object and the value of value"""
         except:
             pass
+        """except is used to catch the error if the file is empty"""
