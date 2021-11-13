32336a2a12090c207e62ae56a3808a702aaed7cc
ralexrivero <ralexrivero@gmail.com>
2021-11-13 14:47:27 -0300

    serialize time format from json

--- models/base_model.py
+++ models/base_model.py
@@ -12,22 +12,31 @@ class BaseModel:
         """ instantiates a new object
             Args:
                 *args: variable length argument list not used
-                **kwargs: (key - value) pair of attributes 
+                **kwargs: (key - value) pair of attributes
             """
-
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    if key != "__class__":
+                        setattr(self, key, value)
         else:
-            kwargs['updated_at'] = datetime.now()
-
+            models.storage.new(self)
 
     def __str__(self):
         """ Returns a string representation of the object """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__)
 
     def save(self):
         """ updates the public instance attribute
@@ -40,7 +49,7 @@ class BaseModel:
         of the instance
         """
         new_dict = dict(self.__dict__)
+        new_dict["created_at"] = self.created_at.isoformat(sep='T')
+        new_dict["updated_at"] = self.updated_at.isoformat(sep='T')
         new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
