123d7836b810dc050e65d1abc600c124b0bccd83
ralexrivero <ralexrivero@gmail.com>
2021-11-13 12:47:19 -0300

    back to old version

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,46 @@
-#!/usr/bin/python3
-""" """
-import uuid
+#!/usr/bin/env python3
+""" BaseModel that defines all common attributes/methods for other classes """
+from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """ """
+    """Base class for all models"""
+
     def __init__(self, *args, **kwargs):
-        """ """
+        """ instantiates a new object
+            Args:
+                *args: variable length argument list not used
+                **kwargs: (key - value) pair of attributes 
+            """
+
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            kwargs['updated_at'] = datetime.now()
+
 
     def __str__(self):
-        """ """
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """ Returns a string representation of the object """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """ """
-        from models import storage
+        """ updates the public instance attribute
+        updated_at with current time"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """ """
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """ Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        new_dict = dict(self.__dict__)
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
