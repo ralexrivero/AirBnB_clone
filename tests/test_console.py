229b0a4613f62fe8e629fb203ca234920265f535
ralexrivero <ralexrivero@gmail.com>
2021-11-13 23:25:14 -0300

    pycodestyle fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,6 +3,13 @@
 Unitest 
 
 Unittest classes:
-    
+    TestCom_prompt
+    TestCom_help
+    TestCom_quit
+    TestCom_create
+    TestCom_show
+    TestCom_all
+    TestCom_update
+    TestCom_destroy
     """
 
