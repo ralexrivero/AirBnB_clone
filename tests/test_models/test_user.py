e0e34700fa2c98b51b206da0386ffd15e15fc2b8
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-14 20:47:34 -0300

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-"""
-Unitest
 
-Unittest classes:
+import unittest
+from models.user import User
+from models.base_model import BaseModel
 
-    """
+
+class testUser(unittest.TestCase):
+    """ unittests for User class """
+    def test_inherit(self):
+        """ checks if it inherits from BaseModel """
+        self.assertTrue(issubclass(User, BaseModel))
