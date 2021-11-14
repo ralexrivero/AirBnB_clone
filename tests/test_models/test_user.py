90ec18c343d628bb08a16e4598d4d861ca2e0a5a
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-14 20:54:10 -0300

    test user mas

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -41,5 +41,25 @@ class testUser(unittest.TestCase):
         string = "[User] ({}) {}".format(user.id, user.__dict__)
         self.assertEqual(string, str(user))
 
+    def test_save(self):
+        """ checks if it saves correctly """
+        user = User()
+        user.save()
+        self.assertNotEqual(user.created_at, user.updated_at)
+
+    def test_to_dict(self):
+        """ checks if it returns correct dictionary """
+        user = User()
+        user_dict = user.to_dict()
+        self.assertEqual(type(user_dict), dict)
+        self.assertTrue("__class__" in user_dict)
+        self.assertTrue("created_at" in user_dict)
+        self.assertTrue("updated_at" in user_dict)
+        self.assertTrue("id" in user_dict)
+        self.assertTrue("email" in user_dict)
+        self.assertTrue("password" in user_dict)
+        self.assertTrue("first_name" in user_dict)
+        self.assertTrue("last_name" in user_dict)
+
 if __name__ == '__main__':
     unittest.main()
