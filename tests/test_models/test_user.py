8c54ccf94c51348841470a00a0ad1dc85c50d856
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-14 20:50:48 -0300

    user test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,3 +10,14 @@ class testUser(unittest.TestCase):
     def test_inherit(self):
         """ checks if it inherits from BaseModel """
         self.assertTrue(issubclass(User, BaseModel))
+
+    def test_attributes(self):
+        """ checks if it has attributes """
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        self.assertTrue(hasattr(user, "password"))
+        self.assertTrue(hasattr(user, "first_name"))
+        self.assertTrue(hasattr(user, "last_name"))
+
+if __name__ == '__main__':
+    unittest.main()
