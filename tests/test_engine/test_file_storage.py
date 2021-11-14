7a2578f3a73b160e2e93caddbe00a255d9fb2802
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-14 16:32:51 -0300

    test storage

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,7 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
-Unitest
+Unitest for the FileStorage class
+"""
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-Unittest classes:
+class TestFileStorage(unittest.TestCase):
 
-    """
+    def test_pep8style(self):
+        """
+        Test that we conform to PEP8
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
