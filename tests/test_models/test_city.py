3c947a499e4d738073184aed166a4ad5bbbccd8d
#RodrigoMato00 <3293@holbertonschol.com>
2021-11-14 21:13:26 -0300

    test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 """
-Unitest
+unittests for the City class
+"""
+import unittest
+from models.city import City
+from models.base_model import BaseModel
 
-Unittest classes:
 
-    """
+class testfile(unittest.TestCase):
+    """ unittests for City class """
+    def test_inheritance(self):
+        """ checks if it inherits from BaseModel """
+        self.assertTrue(issubclass(City, BaseModel))
+
+    def test_attributes(self):
+        """ checks if it has the correct attributes """
+        self.assertTrue('state_id' in City.__dict__)
+        self.assertTrue('name' in City.__dict__)
+
+if __name__ == "__main__":
+    unittest.main()
